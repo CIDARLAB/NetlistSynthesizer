3654496469b8d7498ad0c9a10d48e130bf2571a4
PrashantVaidyanathan <prash@bu.edu>
2013-11-18 21:19:57 -0500

    get file lines of verilog to parse stuff

--- src/ParseVerilog/parseCaseStatements.java
+++ src/ParseVerilog/parseCaseStatements.java
@@ -4,11 +4,64 @@
  */
 package ParseVerilog;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author prashantvaidyanathan
  */
 public class parseCaseStatements {
+    public static int input3case(String Filepath)
+    {
+        
+        
+        File file = new File(Filepath);
+        
+        BufferedReader br;
+        FileReader fr;
+        List<String> filelines = new ArrayList<String>();
+        try
+        {
+            fr = new FileReader(file);
+            br = new BufferedReader(fr);
+            String line;
+            try 
+            {
+                while((line = br.readLine()) != null )
+                {
+                    filelines.add(line.trim());
+                }
+            } 
+            catch (IOException ex) 
+            {
+                Logger.getLogger(parseCaseStatements.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } 
+        catch (FileNotFoundException ex) 
+        {
+            Logger.getLogger(parseCaseStatements.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        int x = 0;
+                
+        for(int i=0;i<filelines.size();i++)
+        {
+            String line = filelines.get(i);
+            if(line.contains("input "))
+            {
+                
+            }
+        }
+        return x;
+    }
     
     
 }
