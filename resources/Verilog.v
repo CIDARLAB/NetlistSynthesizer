2b98496f4d674e7818c6a933e018ad3f8ba1a26b
Prashant Vaidyanathan <prash@bu.edu>
2015-01-01 16:08:43 -0500

    Fixed file links

---
+++ resources/Verilog.v
@@ -0,0 +1,12 @@
+module structuralXOR(output out1,out2, input A, B,C);
+
+wire w1,w2,w3,w4;
+
+
+   not (w1,A);
+   not (w2,B);
+   nor (w3,w1,w2);
+   nor (out1,w3,C);
+   not (out2,out1);
+   
+endmodule
