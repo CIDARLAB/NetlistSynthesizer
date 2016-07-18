21feac3dfb633b3e92e58bce82c8c892998a430b
aonanam <aonanam@bu.edu>
2016-07-18 14:12:48 -0400

    DWire overhaul for MuWire replacement

--- src/main/java/org/cellocad/BU/dom/DWire.java
+++ src/main/java/org/cellocad/BU/dom/DWire.java
@@ -11,59 +11,67 @@ import java.io.Serializable;
  * @author prashantvaidyanathan
  */
 public class DWire implements Serializable {
-   
+
+    public String name;
+    public String logicValue;
+
+    //shane additions start
+    public DGate fromGate;
+    public DGate toGate;
+    public boolean isWritten = false;
+    public DWire dupChannel;
+    public LayerType layer;
+    //shane additions end
     
-     public String name; 
-     public String logicValue;
-     public enum DWireValue{
-         _1,
-         _0,
-         _x;
-     }
-     
-     public DWireValue wValue;
-     public DWireType wtype;
-     int wirestage;
-    
-     public DWire()
-     {
-         name = "";
-         logicValue = "";
-         wValue = DWireValue._x;
-         wtype = DWireType.connector;  
-     }
-     public DWire(String wirename)
-     {
+    public enum DWireValue {
+        _1,
+        _0,
+        _x;
+    }
+
+    public DWireValue wValue;
+    public DWireType wtype;
+    int wirestage;
+
+
+
+    public DWire() {                    //constructing unnamed connector wire
+        name = "";
+        logicValue = "";
+        wValue = DWireValue._x;
+        wtype = DWireType.connector;
+    }
+
+    public DWire(String wirename) {     //constructing connector wire with name
         wtype = DWireType.connector;
         wValue = DWireValue._x;
         logicValue = "";
         this.name = wirename;
-     }
-     public DWire(String wirename,DWireType wType)
-     {
-         if(wType == DWireType.input)
-         {
+    }
+
+    public DWire(String wirename, DWireType wType) {
+        if (wType == DWireType.input) {
             this.wirestage = 0;
-          
-         }
-         this.name = wirename;
-         this.wtype = wType;
-         logicValue = "";
-         wValue = DWireValue._x;
-     }
-     public DWire(DWire xwire)
-     {
+            
+
+        }
+        this.name = wirename;
+        this.wtype = wType;
+        logicValue = "";
+        wValue = DWireValue._x;
+    }
+
+    public DWire(DWire xwire) {
         this.wtype = xwire.wtype;
         this.wValue = xwire.wValue;
         this.name = xwire.name;
         this.logicValue = xwire.logicValue;
-     }
-     
-     public static String printDWire(DWire xwire)
-     {
-         String wstr = "";
-         wstr += ("Wire Name : " + xwire.name);
-         wstr += ("::Wire Type "+ xwire.wtype.toString());
-         return wstr;
-     }
+    }
+
+    public static String printDWire(DWire xwire) {
+        String wstr = "";
+        wstr += ("Wire Name : " + xwire.name);
+        wstr += ("::Wire Type " + xwire.wtype.toString());
+        return wstr;
+    }    
 }
